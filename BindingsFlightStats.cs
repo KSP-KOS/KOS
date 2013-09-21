b6f5cdab42f6a38e4fc057e662fcc049e09af3fb
Eric Oden <oden.eric@gmail.com>
2013-09-21 02:04:50 -0500

    Add boolean INCOMMRANGE. COMMRANGE maybe be redudant now. I can see it being useful for a countdown timer. Maybe add a getter(that's it, says it in the code) for distance from Kerbin. As of now you can only compare it against altitude.

--- BindingsFlightStats.cs
+++ BindingsFlightStats.cs
@@ -21,6 +21,7 @@ namespace kOS
             manager.AddGetter("MISSIONTIME",    delegate(CPU cpu) { return (float)cpu.Vessel.missionTime; });
             manager.AddGetter("STATUS",         delegate(CPU cpu) { return cpu.Vessel.situation.ToString().Replace("_", " "); });
 			manager.AddGetter("COMMRANGE",      delegate(CPU cpu) { return (float)VesselUtils.GetCommRange(cpu.Vessel); });
+			manager.AddGetter("INCOMMRANGE",    delegate(CPU cpu) { return CheckCommRange(cpu.Vessel); });
             manager.AddGetter("APOAPSIS",       delegate(CPU cpu) { return (float)cpu.Vessel.orbit.ApA; });
             manager.AddGetter("PERIAPSIS",      delegate(CPU cpu) { return (float)cpu.Vessel.orbit.PeA; });
 
@@ -143,5 +144,10 @@ namespace kOS
 
             return retVal;
         }
+
+		public bool CheckCommRange(Vessel vessel)
+		{
+			return (VesselUtils.GetDistanceToKerbinSurface(vessel) < VesselUtils.GetCommRange(vessel));
+		}
     }
 }
