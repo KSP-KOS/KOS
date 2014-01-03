b922bf24389ed833afd178fc1b33b8d40b801764
Chris Woerz <chris@metageek.net>
2014-01-03 00:49:39 -0700

    Changed node ETA to be settable

--- Node.cs
+++ Node.cs
@@ -1,27 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace kOS
 {
     public class Node : SpecialValue
     {
-        ManeuverNode nodeRef;
-        Vessel vesselRef;
-        public double UT;
-        public double Pro;
-        public double RadOut;
-        public double Norm;
+        private ManeuverNode nodeRef;
+        private Vessel vesselRef;
 
         public static Dictionary<ManeuverNode, Node> NodeLookup = new Dictionary<ManeuverNode, Node>();
+        public double Time { get; set; }
+        public double Pro { get; set; }
+        public double RadOut { get; set; }
+        public double Norm { get; set; }
 
-        public Node(double ut, double radialOut, double normal, double prograde)
+
+        public Node(double time, double radialOut, double normal, double prograde)
         {
-            this.UT = ut;
-            this.Pro = prograde;
-            this.RadOut = radialOut;
-            this.Norm = normal;
+            Time = time;
+            Pro = prograde;
+            RadOut = radialOut;
+            Norm = normal;
         }
 
         public Node(Vessel v, ManeuverNode existingNode)
@@ -30,22 +28,20 @@ namespace kOS
             vesselRef = v;
             NodeLookup.Add(existingNode, this);
 
-            updateValues();
+            UpdateValues();
         }
 
         public static Node FromExisting(Vessel v, ManeuverNode existingNode)
         {
-            if (NodeLookup.ContainsKey(existingNode)) return NodeLookup[existingNode];
-            
-            return new Node(v, existingNode);
+            return NodeLookup.ContainsKey(existingNode) ? NodeLookup[existingNode] : new Node(v, existingNode);
         }
-        
+
         public void AddToVessel(Vessel v)
         {
             if (nodeRef != null) throw new kOSException("Node has already been added");
 
             vesselRef = v;
-            nodeRef = v.patchedConicSolver.AddManeuverNode(UT);
+            nodeRef = v.patchedConicSolver.AddManeuverNode(Time);
 
             UpdateNodeDeltaV();
 
@@ -56,17 +52,14 @@ namespace kOS
 
         public void UpdateAll()
         {
-            UpdateNodeDeltaV();
-            if (vesselRef != null) vesselRef.patchedConicSolver.UpdateFlightPlan();
+            nodeRef.OnGizmoUpdated(new Vector3d(RadOut, Norm, Pro), Time);
         }
 
         private void UpdateNodeDeltaV()
         {
-            if (nodeRef != null)
-            {
-                Vector3d dv = new Vector3d(RadOut, Norm, Pro);
-                nodeRef.DeltaV = dv;
-            }
+            if (nodeRef == null) return;
+            var dv = new Vector3d(RadOut, Norm, Pro);
+            nodeRef.DeltaV = dv;
         }
 
         public void CheckNodeRef()
@@ -84,38 +77,41 @@ namespace kOS
             return new Vector(nodeRef.GetBurnVector(vesselRef.GetOrbit()));
         }
 
-        private void updateValues()
+        private void UpdateValues()
         {
             // If this node is attached, and the values on the attached node have chaged, I need to reflect that
-            if (nodeRef != null)
-            {
-                UT = nodeRef.UT;
+            if (nodeRef == null) return;
 
-                RadOut = nodeRef.DeltaV.x;
-                Norm = nodeRef.DeltaV.y;
-                Pro = nodeRef.DeltaV.z;
-            }
+            Time = nodeRef.UT;
+            RadOut = nodeRef.DeltaV.x;
+            Norm = nodeRef.DeltaV.y;
+            Pro = nodeRef.DeltaV.z;
         }
                 
         public override object GetSuffix(string suffixName)
         {
-            updateValues();
+            UpdateValues();
             
-            if (suffixName == "BURNVECTOR") return GetBurnVector();
-            else if (suffixName == "ETA") return UT - Planetarium.GetUniversalTime();
-            else if (suffixName == "DELTAV") return GetBurnVector();
-            else if (suffixName == "PROGRADE") return Pro;
-            else if (suffixName == "RADIALOUT") return RadOut;
-            else if (suffixName == "NORMAL") return Norm;
-            else if (suffixName == "APOAPSIS")
+            switch (suffixName)
             {
-                if (nodeRef == null) throw new kOSException("Node must be added to flight plan first");
-                return nodeRef.nextPatch.ApA;
-            }
-            else if (suffixName == "PERIAPSIS")
-            {
-                if (nodeRef == null) throw new kOSException("Node must be added to flight plan first");
-                return nodeRef.nextPatch.PeA;
+                case "BURNVECTOR":
+                    return GetBurnVector();
+                case "ETA":
+                    return Time - Planetarium.GetUniversalTime();
+                case "DELTAV":
+                    return GetBurnVector();
+                case "PROGRADE":
+                    return Pro;
+                case "RADIALOUT":
+                    return RadOut;
+                case "NORMAL":
+                    return Norm;
+                case "APOAPSIS":
+                    if (nodeRef == null) throw new kOSException("Node must be added to flight plan first");
+                    return nodeRef.nextPatch.ApA;
+                case "PERIAPSIS":
+                    if (nodeRef == null) throw new kOSException("Node must be added to flight plan first");
+                    return nodeRef.nextPatch.PeA;
             }
 
             return base.GetSuffix(suffixName);
@@ -123,31 +119,49 @@ namespace kOS
 
         public override bool SetSuffix(string suffixName, object value)
         {
-            if (suffixName == "BURNVECTOR" || suffixName == "ETA" || suffixName == "DELTAV") throw new kOSReadOnlyException(suffixName);
+            UpdateValues();
 
-            else if (suffixName == "PROGRADE") { Pro = (double)value; UpdateAll(); return true; }
-            else if (suffixName == "RADIALOUT") { RadOut = (double)value; UpdateAll(); return true; }
-            else if (suffixName == "NORMAL") { Norm = (double)value; UpdateAll(); return true; }
+            switch (suffixName)
+            {
+                case "DELTAV":
+                case "BURNVECTOR":
+                    throw new kOSReadOnlyException(suffixName);
+                case "ETA":
+                    Time = ((double)value) + Planetarium.GetUniversalTime();
+                    UpdateAll();
+                    return true;
+                case "PROGRADE":
+                    Pro = (double)value;
+                    UpdateAll();
+                    return true;
+                case "RADIALOUT":
+                    RadOut = (double)value;
+                    UpdateAll();
+                    return true;
+                case "NORMAL":
+                    Norm = (double)value;
+                    UpdateAll();
+                    return true;
+            }
 
             return false;
         }
 
         public void Remove()
         {
-            if (nodeRef != null)
-            {
-                NodeLookup.Remove(nodeRef);
+            if (nodeRef == null) return;
 
-                vesselRef.patchedConicSolver.RemoveManeuverNode(nodeRef);
+            NodeLookup.Remove(nodeRef);
 
-                nodeRef = null;
-                vesselRef = null;
-            }
+            vesselRef.patchedConicSolver.RemoveManeuverNode(nodeRef);
+
+            nodeRef = null;
+            vesselRef = null;
         }
 
         public override string ToString()
         {
-            return "NODE(" + UT + "," + RadOut + "," + Norm + "," + Pro + ")";
+            return "NODE(" + Time + "," + RadOut + "," + Norm + "," + Pro + ")";
         }
     }
 }
