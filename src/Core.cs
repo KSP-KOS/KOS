164b550434fa26a8b6ea060d216462af05760e00
Chris Woerz <erendrake@gmail.com>
2014-05-21 07:17:55 -0600

    reved to 12.2 resolves #113

--- src/Core.cs
+++ src/Core.cs
@@ -10,7 +10,7 @@ namespace kOS
 {
     public class Core : MonoBehaviour
     {
-        public static VersionInfo VersionInfo = new VersionInfo(0, 12.0);
+        public static VersionInfo VersionInfo = new VersionInfo(0, 12.2);
 
         public static Core Fetch; 
         public TermWindow Window;
