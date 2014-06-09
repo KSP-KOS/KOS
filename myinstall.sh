5c1c3df97ace69ae90e11c58a0a3d02d4964d2c4
Dunbaratu <madings@gmail.com>
2014-06-09 00:31:28 -0500

    Previous commit overzealously committed too much, including some of my own script files that aren't part of the KOS source.  Went back and "git rm --cached" them.

--- myinstall.sh
+++
@@ -1,5 +0,0 @@
-KOSDLL="../../../../../Program Files (x86)/Steam/steamapps/common/Kerbal Space Program/GameData/kOS/Plugins/kOS.dll"
-BUILDDLL=src/bin/Debug/kOS.dll
-
-cp "$BUILDDLL" "$KOSDLL"
-ls -l "$BUILDDLL" "$KOSDLL"
