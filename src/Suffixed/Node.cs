dd21653070bb971ecbfe38f7bacd8aceabc17df5
Chris Woerz <chris@metageek.net>
2014-06-30 23:26:30 -0600

    merged #176

--- src/Suffixed/Node.cs
+++ src/Suffixed/Node.cs
@@ -5,25 +5,37 @@ namespace kOS.Suffixed
 {
     public class Node : SpecialValue
     {
-        public static Dictionary<ManeuverNode, Node> NodeLookup { get; private set; } 
+        private static readonly Dictionary<ManeuverNode, Node> nodeLookup;
 
         private ManeuverNode nodeRef;
         private Vessel vesselRef;
         private readonly SharedObjects shared;
+        private double time;
+        private double pro;
+        private double radOut;
+        private double norm;
 
-        public Node(double time, double radialOut, double normal, double prograde, SharedObjects shareObj) :this (shareObj)
+        static Node()
         {
-            Time = time;
-            Pro = prograde;
-            RadOut = radialOut;
-            Norm = normal;
+            nodeLookup = new Dictionary<ManeuverNode, Node>();
         }
 
-        public Node(Vessel v, ManeuverNode existingNode, SharedObjects shareObj) :this(shareObj)
+        public Node(double time, double radialOut, double normal, double prograde, SharedObjects shareObj)
+            : this(shareObj)
+        {
+            this.time = time;
+            pro = prograde;
+            radOut = radialOut;
+            norm = normal;
+        }
+
+        private Node(Vessel v, ManeuverNode existingNode, SharedObjects shareObj)
+            : this(shareObj)
         {
             nodeRef = existingNode;
             vesselRef = v;
-            NodeLookup.Add(existingNode, this);
+
+            nodeLookup.Add(existingNode, this);
 
             UpdateValues();
         }
@@ -33,53 +45,23 @@ namespace kOS.Suffixed
             this.shared = shared;
         }
 
-        public double Time { get; set; }
-        public double Pro { get; set; }
-        public double RadOut { get; set; }
-        public double Norm { get; set; }
-
-        static Node()
-        {
-            NodeLookup = new Dictionary<ManeuverNode, Node>();
-        }
-
         public static Node FromExisting(Vessel v, ManeuverNode existingNode, SharedObjects shared)
         {
-            return NodeLookup.ContainsKey(existingNode) ? NodeLookup[existingNode] : new Node(v, existingNode, shared);
+            return nodeLookup.ContainsKey(existingNode) ? nodeLookup[existingNode] : new Node(v, existingNode, shared);
         }
 
         public void AddToVessel(Vessel v)
         {
             if (nodeRef != null) throw new Exception("Node has already been added");
-            
+
             vesselRef = v;
-            nodeRef = v.patchedConicSolver.AddManeuverNode(Time);
+            nodeRef = v.patchedConicSolver.AddManeuverNode(time);
 
             UpdateNodeDeltaV();
 
             v.patchedConicSolver.UpdateFlightPlan();
 
-            NodeLookup.Add(nodeRef, this);
-        }
-
-        public void UpdateAll()
-        {
-            nodeRef.OnGizmoUpdated(new Vector3d(RadOut, Norm, Pro), Time);
-        }
-
-        private void UpdateNodeDeltaV()
-        {
-            if (nodeRef == null) return;
-            var dv = new Vector3d(RadOut, Norm, Pro);
-            nodeRef.DeltaV = dv;
-        }
-
-        public void CheckNodeRef()
-        {
-            if (nodeRef == null)
-            {
-                throw new Exception("Must attach node first");
-            }
+            nodeLookup.Add(nodeRef, this);
         }
 
         public Vector GetBurnVector()
@@ -87,17 +69,6 @@ namespace kOS.Suffixed
             CheckNodeRef();
 
             return new Vector(nodeRef.GetBurnVector(vesselRef.GetOrbit()));
-        }
-
-        private void UpdateValues()
-        {
-            // If this node is attached, and the values on the attached node have chaged, I need to reflect that
-            if (nodeRef == null) return;
-
-            Time = nodeRef.UT;
-            RadOut = nodeRef.DeltaV.x;
-            Norm = nodeRef.DeltaV.y;
-            Pro = nodeRef.DeltaV.z;
         }
 
         public override object GetSuffix(string suffixName)
@@ -109,14 +80,19 @@ namespace kOS.Suffixed
                 case "DELTAV":
                 case "BURNVECTOR":
                     return GetBurnVector();
+
                 case "ETA":
-                    return Time - Planetarium.GetUniversalTime();
+                    return time - Planetarium.GetUniversalTime();
+
                 case "PROGRADE":
-                    return Pro;
+                    return pro;
+
                 case "RADIALOUT":
-                    return RadOut;
+                    return radOut;
+
                 case "NORMAL":
-                    return Norm;
+                    return norm;
+
                 case "ORBIT":
                     if (nodeRef == null) throw new Exception("Node must be added to flight plan first");
                     return new OrbitInfo(nodeRef.nextPatch, shared);
@@ -137,17 +113,21 @@ namespace kOS.Suffixed
                 case "ORBIT":
                     throw new Exception(string.Format("Suffix {0} is read only!", suffixName));
                 case "ETA":
-                    Time = ((double) value) + Planetarium.GetUniversalTime();
+                    time = ((double)value) + Planetarium.GetUniversalTime();
                     break;
+
                 case "PROGRADE":
-                    Pro = (double) value;
+                    pro = (double)value;
                     break;
+
                 case "RADIALOUT":
-                    RadOut = (double) value;
+                    radOut = (double)value;
                     break;
+
                 case "NORMAL":
-                    Norm = (double) value;
+                    norm = (double)value;
                     break;
+
                 default:
                     return false;
             }
@@ -159,7 +139,7 @@ namespace kOS.Suffixed
         {
             if (nodeRef == null) return;
 
-            NodeLookup.Remove(nodeRef);
+            nodeLookup.Remove(nodeRef);
 
             vesselRef.patchedConicSolver.RemoveManeuverNode(nodeRef);
 
@@ -170,7 +150,38 @@ namespace kOS.Suffixed
         public override string ToString()
         {
             UpdateValues();
-            return "NODE(" + Time + "," + RadOut + "," + Norm + "," + Pro + ")";
+            return "NODE(" + time + "," + radOut + "," + norm + "," + pro + ")";
+        }
+
+        private void UpdateAll()
+        {
+            nodeRef.OnGizmoUpdated(new Vector3d(radOut, norm, pro), time);
+        }
+
+        private void UpdateNodeDeltaV()
+        {
+            if (nodeRef == null) return;
+            var dv = new Vector3d(radOut, norm, pro);
+            nodeRef.DeltaV = dv;
+        }
+
+        private void CheckNodeRef()
+        {
+            if (nodeRef == null)
+            {
+                throw new Exception("Must attach node first");
+            }
+        }
+
+        private void UpdateValues()
+        {
+            // If this node is attached, and the values on the attached node have chaged, I need to reflect that
+            if (nodeRef == null) return;
+
+            time = nodeRef.UT;
+            radOut = nodeRef.DeltaV.x;
+            norm = nodeRef.DeltaV.y;
+            pro = nodeRef.DeltaV.z;
         }
     }
 }
