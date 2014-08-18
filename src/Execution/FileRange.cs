1be208f2c13888f273a2809613870f6a59ef8953
Chris Woerz <chris@metageek.net>
2014-08-18 15:13:58 -0600

    prune unused file

--- src/Execution/FileRange.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace kOS.Execution
-{
-    public struct FileRange
-    {
-        public string Filename { get; set; }
-        public int Start { get; set; }
-        public int Stop { get; set; }
-    }
-}
