a94c5f4ded7f1bcb0ba3e9a6db320e781b7a65be
Chris Woerz <chris@metageek.net>
2014-09-22 09:41:59 -0600

    added new quicksave/load syntax

--- src/kOS/Binding/BindingsUniverse.cs
+++ src/kOS/Binding/BindingsUniverse.cs
@@ -1,20 +1,65 @@
-﻿using System;
+﻿using kOS.Execution;
 using kOS.Suffixed;
-using kOS.Execution;
+using System;
+using System.Collections.Generic;
 
 namespace kOS.Binding
 {
     [kOSBinding("ksp")]
     public class BindingTimeWarp : Binding
     {
+        private static readonly IList<string> reservedSaveNames = new List<string> { "persistence", "quicksave" };
+
         public override void AddTo(SharedObjects shared)
         {
             Shared = shared;
 
-            Shared.BindingMgr.AddGetter("LOADDISTANCE", cpu => Vessel.loadDistance );
+            Shared.BindingMgr.AddGetter("QUICKSAVE", cpu =>
+            {
+                if (!HighLogic.CurrentGame.Parameters.Flight.CanQuickSave) return false;
+                QuickSaveLoad.QuickSave();
+                return true;
+            });
+            
+            Shared.BindingMgr.AddGetter("QUICKLOAD", cpu =>
+                {
+                    if (!HighLogic.CurrentGame.Parameters.Flight.CanQuickLoad) return false;
+                    try
+                    {
+                        GamePersistence.LoadGame("quicksave", HighLogic.SaveFolder, true, false);
+                    }
+                    catch (Exception ex)
+                    {
+                        UnityEngine.Debug.Log(ex.Message);
+                        return false;
+                    }
+                    return true;
+                });
+
+            Shared.BindingMgr.AddSetter("SAVETO", (cpu, val) =>
+                {
+                    if (reservedSaveNames.Contains(val.ToString().ToLower())) return; 
+
+                    Game game = HighLogic.CurrentGame.Updated();
+                    game.startScene = GameScenes.FLIGHT;
+                    GamePersistence.SaveGame(game, val.ToString(), HighLogic.SaveFolder, SaveMode.OVERWRITE);
+                });
+
+            Shared.BindingMgr.AddSetter("LOADFROM", (cpu, val) =>
+                {
+                    if (reservedSaveNames.Contains(val.ToString().ToLower())) return;
+
+                    var game = GamePersistence.LoadGame(val.ToString(), HighLogic.SaveFolder, true, false);
+                    if (game == null) return;
+                    if (game.flightState == null) return;
+                    if (!game.compatible) return;
+                    FlightDriver.StartAndFocusVessel(game, game.flightState.activeVesselIdx);
+                });
+
+            Shared.BindingMgr.AddGetter("LOADDISTANCE", cpu => Vessel.loadDistance);
             Shared.BindingMgr.AddSetter("LOADDISTANCE", delegate(CPU cpu, object val)
                 {
-                    var distance = (float) val;
+                    var distance = Convert.ToSingle(val);
                     Vessel.loadDistance = distance;
                     Vessel.unloadDistance = distance - 250;
                 });
@@ -24,8 +69,10 @@ namespace kOS.Binding
                     {
                         case TimeWarp.Modes.HIGH:
                             return "RAILS";
+
                         case TimeWarp.Modes.LOW:
                             return "PHYSICS";
+
                         default:
                             throw new ArgumentOutOfRangeException();
                     }
@@ -39,9 +86,11 @@ namespace kOS.Binding
                         case "PHYSICS":
                             toSet = TimeWarp.Modes.LOW;
                             break;
+
                         case "RAILS":
                             toSet = TimeWarp.Modes.HIGH;
                             break;
+
                         default:
                             throw new Exception(string.Format("WARPMODE '{0}' is not valid", val));
                     }
@@ -60,7 +109,7 @@ namespace kOS.Binding
             Shared.BindingMgr.AddGetter("MAPVIEW", cpu => MapView.MapIsEnabled);
             Shared.BindingMgr.AddSetter("MAPVIEW", delegate(CPU cpu, object val)
                 {
-                    if( Convert.ToBoolean( val ) )
+                    if (Convert.ToBoolean(val))
                     {
                         MapView.EnterMapView();
                     }
