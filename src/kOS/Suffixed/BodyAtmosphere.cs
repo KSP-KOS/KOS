cd89a90c6b5e07aa4808fac6699d9ee06407531f
Chris Woerz <chris@metageek.net>
2014-09-29 12:08:32 -0600

    fixed horrible variable name

--- src/kOS/Suffixed/BodyAtmosphere.cs
+++ src/kOS/Suffixed/BodyAtmosphere.cs
@@ -4,23 +4,23 @@ namespace kOS.Suffixed
 {
     public class BodyAtmosphere : Structure
     {
-        private readonly CelestialBody b;
+        private readonly CelestialBody celestialBody;
 
-        public BodyAtmosphere(CelestialBody b)
+        public BodyAtmosphere(CelestialBody celestialBody)
         {
-            this.b = b;
+            this.celestialBody = celestialBody;
 
-            AddSuffix("BODY", new Suffix<CelestialBody,string>(b, model => model.bodyName));
-            AddSuffix("EXISTS", new Suffix<CelestialBody,bool>(b, model => model.atmosphere));
-            AddSuffix("OXYGEN", new Suffix<CelestialBody,bool>(b, model => b.atmosphere && b.atmosphereContainsOxygen));
-            AddSuffix("SCALE", new Suffix<CelestialBody,double>(b, model => b.atmosphere ? b.atmosphereScaleHeight : 0));
-            AddSuffix("SEALEVELPRESSURE", new Suffix<CelestialBody,double>(b, model => b.atmosphere ? b.staticPressureASL : 0));
-            AddSuffix("HEIGHT", new Suffix<CelestialBody,double>(b, model => b.atmosphere ? b.maxAtmosphereAltitude : 0));
+            AddSuffix("BODY", new Suffix<CelestialBody,string>(celestialBody, model => model.bodyName));
+            AddSuffix("EXISTS", new Suffix<CelestialBody,bool>(celestialBody, model => model.atmosphere));
+            AddSuffix("OXYGEN", new Suffix<CelestialBody,bool>(celestialBody, model => celestialBody.atmosphere && celestialBody.atmosphereContainsOxygen));
+            AddSuffix("SCALE", new Suffix<CelestialBody,double>(celestialBody, model => celestialBody.atmosphere ? celestialBody.atmosphereScaleHeight : 0));
+            AddSuffix("SEALEVELPRESSURE", new Suffix<CelestialBody,double>(celestialBody, model => celestialBody.atmosphere ? celestialBody.staticPressureASL : 0));
+            AddSuffix("HEIGHT", new Suffix<CelestialBody,double>(celestialBody, model => celestialBody.atmosphere ? celestialBody.maxAtmosphereAltitude : 0));
         }
 
         public override string ToString()
         {
-            return "BODYATMOSPHERE(\"" + b.bodyName + "\")";
+            return "BODYATMOSPHERE(\"" + celestialBody.bodyName + "\")";
         }
     }
 }
