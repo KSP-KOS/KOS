dc2060c4b6012b8c4c1353ad8a8e5f4d6efde292
Chris Woerz <erendrake@gmail.com>
2014-11-11 12:51:08 -0700

    moved what i could of sharedobjects to safe

---
+++ src/kOS.Safe/SharedObjects.cs
@@ -0,0 +1,18 @@
+using kOS.Safe.Compilation;
+using kOS.Safe.Execution;
+using kOS.Safe.Module;
+using kOS.Safe.Screen;
+
+namespace kOS.Safe
+{
+    public class SharedObjects
+    {
+        public ICpu Cpu { get; set; }
+        public IScreenBuffer Screen { get; set; }
+        public IInterpreter Interpreter { get; set; }
+        public Script ScriptHandler { get; set; }
+        public ILogger Logger { get; set; }
+        public IProcessor Processor { get; set; }
+        public UpdateHandler UpdateHandler { get; set; }
+    }
+}
