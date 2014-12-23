fbded3c1104b95f72af90f44f6e0be4f08ac3066
Chris Woerz <erendrake@gmail.com>
2014-12-23 13:05:31 -0700

    split resources structure for single part and aggregate

---
+++ src/kOS/Suffixed/SingleResourceValue.cs
@@ -0,0 +1,34 @@
+﻿using kOS.Safe.Encapsulation;
+using kOS.Safe.Encapsulation.Suffixes;
+using kOS.Suffixed.Part;
+
+namespace kOS.Suffixed
+{
+    public class SingleResourceValue : Structure
+    {
+        private readonly PartResource partResource;
+
+        public SingleResourceValue(PartResource partResource)
+        {
+            this.partResource = partResource;
+            InitializeSuffixes();
+        }
+
+        private void InitializeSuffixes()
+        {
+            AddSuffix("NAME", new Suffix<string>(() => partResource.resourceName));
+            AddSuffix("AMOUNT", new Suffix<double>(() => partResource.amount));
+            AddSuffix("DENSITY", new Suffix<double>(() => partResource.info.density));
+            AddSuffix("UNITCOST", new Suffix<double>(() => partResource.info.unitCost));
+            AddSuffix("CAPACITY", new Suffix<double>(() => partResource.maxAmount));
+            AddSuffix("TWEAKABLE", new Suffix<bool>(() => partResource.isTweakable));
+            AddSuffix("FLOWMODE", new Suffix<string>(() => partResource.flowMode.ToString()));
+            AddSuffix("FLOWSTATE", new SetSuffix<bool>(() => partResource.flowState, value => partResource.flowState = value));
+        }
+
+        public override string ToString()
+        {
+            return string.Format("RESOURCE({0},{1},{2}", partResource.resourceName, partResource.amount, partResource.maxAmount);
+        }
+    }
+}
