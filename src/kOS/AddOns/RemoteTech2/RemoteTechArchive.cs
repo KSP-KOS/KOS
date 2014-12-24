97b57d673d3dfb1b5d0f21ce0e3411aac5e9cd3d
hvacengi <hvacengi@gmail.com>
2014-12-23 22:39:01 -0500

    Workaround to fix #448 and maybe #428
    RemoteTechArchive.cs - Modified CheckRange to return true if the vessel
    is currently in the prelaunch condition.  This prevents the archive from
    showing as out of comm range during initial start up when RemoteTech
    seems to have issues returning the correct value reliably.
    BindingManager.cs - Added a null check before disposing the
    flightControl variable.  I have had issues on and off with this throwing
    null reference errors when reverting or going to the space center.

--- src/kOS/AddOns/RemoteTech2/RemoteTechArchive.cs
+++ src/kOS/AddOns/RemoteTech2/RemoteTechArchive.cs
@@ -7,7 +7,9 @@ namespace kOS.AddOns.RemoteTech2
     {
         public bool CheckRange(Vessel vessel)
         {
-            return vessel != null && RemoteTechHook.Instance.HasConnectionToKSC(vessel.id);
+            // return true if RemoteTech reports a connection to KSC, or if the vessel is currently 
+            // in "PRELAUNCH" situation, which converts to int 2
+            return vessel != null && (RemoteTechHook.Instance.HasConnectionToKSC(vessel.id) || (int)vessel.situation == 2);
         }
     }
 }
