c8a420b9f1adb4a6a7bb7f923301a6fbf0376814
pakrym <pavel@krymets.com>
2014-12-24 22:57:20 +0200

    Fixes revert flight after syntax error in boot...
    ... file.
    Adds terminal window open close bindings

--- src/kOS/Binding/TerminalSettings.cs
+++ src/kOS/Binding/TerminalSettings.cs
@@ -1,4 +1,5 @@
-﻿using kOS.Safe.Binding;
+﻿using System;
+using kOS.Safe.Binding;
 
 namespace kOS.Binding
 {
@@ -7,6 +8,20 @@ namespace kOS.Binding
     {
         public override void AddTo(SharedObjects shared)
         {
+
+            shared.BindingMgr.AddSetter("TERMINAL", (value) =>
+            {
+                bool open;
+                if (Boolean.TryParse(value.ToString(), out open))
+                {
+                    if (open)
+                        shared.Window.Open();
+                    else
+                        shared.Window.Close();
+
+                }
+            });
+            shared.BindingMgr.AddGetter("TERMINAL", () => shared.Window.IsOpen());
             shared.BindingMgr.AddGetter("SESSIONTIME", () => shared.Cpu.SessionTime);
             shared.BindingMgr.AddGetter("VERSION", () => Core.VersionInfo);
             shared.BindingMgr.AddGetter("VOLUME:NAME", () => shared.VolumeMgr.CurrentVolume.Name);
