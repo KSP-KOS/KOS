d2dc04b32001aa4abfbe4c56263da60091bcb5eb
Chris Woerz <erendrake@gmail.com>
2015-02-12 21:39:00 -0700

    added step to clamp suffix

--- src/kOS.Safe/Encapsulation/Suffixes/ClampSetSuffix.cs
+++ src/kOS.Safe/Encapsulation/Suffixes/ClampSetSuffix.cs
@@ -2,10 +2,17 @@ using kOS.Safe.Utilities;
 
 namespace kOS.Safe.Encapsulation.Suffixes
 {
-    public class ClampSetSuffix<TValue> : SetSuffix<TValue> 
+    public class ClampSetSuffix<TValue> : SetSuffix<TValue>
     {
         private readonly double min;
         private readonly double max;
+        private readonly float stepIncrement;
+
+        public ClampSetSuffix(SuffixGetDlg<TValue> getter, SuffixSetDlg<TValue> setter, double min, double max, float stepIncrement, string description = "") 
+            : this(getter, setter, min, max, description)
+        {
+            this.stepIncrement = stepIncrement;
+        }
 
         public ClampSetSuffix(SuffixGetDlg<TValue> getter, SuffixSetDlg<TValue> setter, double min, double max, string description = "")
             : base(getter, setter, description)
@@ -16,8 +23,16 @@ namespace kOS.Safe.Encapsulation.Suffixes
 
         public override void Set(object value)
         {
-            //HACK, this is assumes the value parses as a double 
-            base.Set(Math.Clamp(double.Parse(value.ToString()), min, max));
+            //HACK, this is assumes the value parses as a double
+            var dblValue = double.Parse(value.ToString());
+            
+            //Rounding to the nearest step
+            if (!(System.Math.Abs(stepIncrement) < 0.00001))
+            {
+                dblValue = System.Math.Round(dblValue/stepIncrement)*stepIncrement;
+            }
+
+            base.Set(Math.Clamp(dblValue, min, max));
         }
     }
 }
