71c88ee3c1723dfaa9efb0ff183eb133c3a5c773
Chris Woerz <erendrake@gmail.com>
2015-09-21 14:52:19 -0600

    more changes from review

--- src/kOS.Safe/Exceptions/KOSDuplicateKeyException.cs
+++ src/kOS.Safe/Exceptions/KOSDuplicateKeyException.cs
@@ -2,7 +2,7 @@
 {
     public class KOSDuplicateKeyException : KOSException
     {
-        private const string MSG = "The given key {0} is already present, use CONTAINS for checking safety. This is a case {1} collection";
+        private const string MSG = "The given key {0} is already present in the case-{1} collection. Use CONTAINS for checking safety.";
 
         public KOSDuplicateKeyException(string key, bool caseSensitive) : base(string.Format(MSG, key, caseSensitive ? "insensitive" : "sensitive"))
         {
