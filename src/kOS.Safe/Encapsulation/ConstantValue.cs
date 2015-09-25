df431697ccc2e8deb0e357c9da9af9596869dd1d
Dunbaratu <madings@gmail.com>
2015-09-24 23:33:02 -0500

    Made constant() work without the parens, and added a few more constants to it.

--- src/kOS.Safe/Encapsulation/ConstantValue.cs
+++ src/kOS.Safe/Encapsulation/ConstantValue.cs
@@ -9,8 +9,20 @@ namespace kOS.Safe.Encapsulation
         {
             AddGlobalSuffix<ConstantValue>("G", new StaticSuffix<double>(() => 6.67384*Math.Pow(10,-11)));
             AddGlobalSuffix<ConstantValue>("E", new StaticSuffix<double>(() => Math.E));
-            AddGlobalSuffix<ConstantValue>("e", new StaticSuffix<double>(() => Math.E));
             AddGlobalSuffix<ConstantValue>("PI", new StaticSuffix<double>(() => Math.PI));
+            AddGlobalSuffix<ConstantValue>("C", new StaticSuffix<double>(() => 299792458.0)); // spd of light in m/s.
+            
+            // atmospheres to kiloPascals
+            AddGlobalSuffix<ConstantValue>("ATMTOKPA", new StaticSuffix<double>(() => 101.325));
+
+            // kiloPascals to atmospheres
+            AddGlobalSuffix<ConstantValue>("KPATOATM", new StaticSuffix<double>(() => 0.00986923266716012830002467308167));
+
+            // pi/180 :
+            AddGlobalSuffix<ConstantValue>("DEGTORAD", new StaticSuffix<double>(() => 0.01745329251994329576923690768489));
+            
+            // 180/pi :
+            AddGlobalSuffix<ConstantValue>("RADTODEG", new StaticSuffix<double>(() => 57.295779513082320876798154814105));
         }
 
         public override string ToString()
