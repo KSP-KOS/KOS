8d79ef8b939e5b9ea850eb95580954e3d17683c2
Chris Woerz <erendrake@gmail.com>
2015-09-28 15:55:47 -0600

    updated exception names

--- src/kOS.Safe/Exceptions/KOSCommandInvalidHereException.cs
+++ src/kOS.Safe/Exceptions/KOSCommandInvalidHereException.cs
@@ -8,7 +8,7 @@ namespace kOS.Safe.Exceptions
     /// semantic reason that it can't be used WHERE it's being used.
     /// Example:  Trying to PRESERVE when *not* in a trigger body.
     /// </summary>
-    public class KOSCommandInvalidHere: KOSCompileException
+    public class KOSCommandInvalidHereException : KOSCompileException
     {
         private const string TERSE_MSG_FMT = "'{0}' command found {1}. It only works {2}.";
 
@@ -19,7 +19,8 @@ namespace kOS.Safe.Exceptions
             "places of the code.  This is one of those cases.\n";
 
         // Just nothing by default:
-        public override string HelpURL { get{ return "";} }
+        public override string HelpURL { get { return ""; } }
+
         public override string VerboseMessage { get { return VerbosePrefix; } }
 
         /// <summary>
@@ -30,8 +31,8 @@ namespace kOS.Safe.Exceptions
         /// Use a phrasing that starts with a preposition, i.e. "in a loop", "outside a loop"</param>
         /// <param name="goodPlace">describing what sort of code the it is meant to be used in instead.
         /// Use a phrasing that starts with a preposition, i.e. "in a loop", "outside a loop"</param>
-        public KOSCommandInvalidHere(string command, string badPlace, string goodPlace) :
-            base(String.Format(TERSE_MSG_FMT, command, badPlace, goodPlace))
+        public KOSCommandInvalidHereException(string command, string badPlace, string goodPlace) :
+            base(string.Format(TERSE_MSG_FMT, command, badPlace, goodPlace))
         {
         }
     }
--- src/kOS.Safe/Exceptions/KOSInvalidDelegateContextException.cs
+++ src/kOS.Safe/Exceptions/KOSInvalidDelegateContextException.cs
@@ -1,24 +1,22 @@
-﻿using System;
-
-namespace kOS.Safe.Exceptions
+﻿namespace kOS.Safe.Exceptions
 {
     /// <summary>
     /// Thrown when you attempt to call a user delegate from an invalid context
     /// where it cannot access the delegate from.
     /// </summary>
-    public class KOSInvalidDelegateContext : KOSException
+    public class KOSInvalidDelegateContextException : KOSException
     {
         private const string TERSE_MSG_FMT = "Cannot call this lock or function from {0} when it was declared in {1}.";
         private const string HELP_URL = "http://ksp-kos.github.io/KOS_DOC/language/user_functions.html#functions-and-the-terminal-interpreter";
-        
-        public KOSInvalidDelegateContext(string currentContextName, string intendedContextName) :
-            base(String.Format(TERSE_MSG_FMT, currentContextName, intendedContextName))
+
+        public KOSInvalidDelegateContextException(string currentContextName, string intendedContextName) :
+            base(string.Format(TERSE_MSG_FMT, currentContextName, intendedContextName))
         {
         }
 
         public override string VerboseMessage
         {
-            get { return base.Message; }
+            get { return Message; }
         }
 
         public override string HelpURL
