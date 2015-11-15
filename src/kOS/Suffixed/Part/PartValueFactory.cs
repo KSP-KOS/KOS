fb98d3094ec8bb115e9a29856fb8f52cc24940d0
AlchemistCH <HimikCH@mail.ru>
2015-11-15 11:46:24 +0200

    Update PartValueFactory.cs

--- src/kOS/Suffixed/Part/PartValueFactory.cs
+++ src/kOS/Suffixed/Part/PartValueFactory.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using kOS.Safe.Encapsulation;
 
@@ -20,6 +20,12 @@ namespace kOS.Suffixed.Part
 
         public static PartValue Construct(global::Part part, SharedObjects shared)
         {
+            foreach (PartModule module in part.Modules) //Check for mutimode engine (let's not run into usual engine modules first)
+            {
+                MultiModeEngine mmEng = module as MultiModeEngine;
+                if (mmEng != null)
+                    return new EngineValue(part, mmEng, shared);
+            }
             foreach (PartModule module in part.Modules)
             {
                 ModuleEngines mEng = module as ModuleEngines;
@@ -33,13 +39,15 @@ namespace kOS.Suffixed.Part
                     return new DockingPortValue(mDock, shared);
                 ModuleEnviroSensor mSense = module as ModuleEnviroSensor;
                 if (mSense != null)
-                    return new SensorValue(part, mSense, shared);
-                var gimbalModule  = module as ModuleGimbal;
-                if (gimbalModule != null)
-                    return new GimbalValue(gimbalModule,shared);
-                
+                    return new SensorValue(part, mSense, shared);     
             }
-            
+            foreach (PartModule module in part.Modules) //gimbal is checked after engines not found
+            {
+                var gimbalModule = module as ModuleGimbal;
+                if (gimbalModule != null)
+                    return new GimbalValue(gimbalModule, shared);
+            }
+
             // Fallback if none of the above: then just a normal part:
             return new PartValue(part, shared);
         }
