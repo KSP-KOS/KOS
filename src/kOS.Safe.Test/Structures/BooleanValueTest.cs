c40eaddd6c266736dd91dc8bbb82193e827e3dd5
Brad White <hvacengi@gmail.com>
2015-12-10 10:06:18 -0500

    Unit tests
    BooleanValueTest.cs
    * Add unit tests for new BooleanValue type
    kOS.Safe.Test.csproj
    * Add reference to new tests.

---
+++ src/kOS.Safe.Test/Structures/BooleanValueTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using kOS.Safe.Encapsulation;
+using NUnit.Framework;
+
+namespace kOS.Safe.Test.Structures
+{
+    [TestFixture]
+    public class BooleanValueTest
+    {
+        [Test]
+        public void CanConstructFromBool()
+        {
+            bool b = true;
+            BooleanValue bv = new BooleanValue(b);
+            Assert.AreEqual(b, bv.Value);
+            Assert.IsTrue(b == bv);
+            Assert.IsFalse(b == !bv);
+            Assert.IsFalse(b != bv);
+            Assert.IsFalse(!b == bv);
+        }
+        [Test]
+        public void CanImplicitlyConvertToBool()
+        {
+            bool b = true;
+            BooleanValue bv = new BooleanValue(b);
+            bool b2 = bv;
+            Assert.AreEqual(b, b2);
+        }
+    }
+}
