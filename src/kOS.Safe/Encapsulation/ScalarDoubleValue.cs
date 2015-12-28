39d021f662627dda712c6947ddee780b09ca37cf
Brad White <hvacengi@gmail.com>
2015-12-28 10:45:51 -0500

    Move subclasses to their own files
    ScalarValue.cs
    * Made internalValue a private field
    * Exposed a protected setter on Value to set the internal value.
    * Removed the subclasses
    ScalarIntValue.cs
    * Move ScalarIntValue into it's own file
    ScalarDoubleValue
    * Move ScalarDoubleValue into it's own file
    kOS.Safe.Csproj
    * reference the new files detailed above.

---
+++ src/kOS.Safe/Encapsulation/ScalarDoubleValue.cs
@@ -0,0 +1,20 @@
+﻿namespace kOS.Safe.Encapsulation
+{
+    public class ScalarDoubleValue : ScalarValue
+    {
+        public override bool IsDouble
+        {
+            get { return true; }
+        }
+
+        public override bool IsInt
+        {
+            get { return false; }
+        }
+
+        public ScalarDoubleValue(double value)
+        {
+            Value = value;
+        }
+    }
+}
