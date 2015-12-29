322a96bafa2eba4621cb92a84e99ff08cee22fab
hvacengi <hvacengi@gmail.com>
2015-12-28 23:12:37 -0500

    1st pass at implementing kOSVesselModule
    kOSVesselModule.cs
    * Implement new vessel module to manage vessel specific values
    * Manage steering manager
    IFlightControlParameter.cs
    * New interface for flight controls
    kOS.csproj
    * Reference the new files
    FlightControlManager.cs
    * Stop using SteeringManagerProvider, and reference the new vessel
    module instead
    FlightStats.cs
    * Get the STEERINGMANGER from the vessel module instead of the provider
    SteeringManager.cs
    * Implement IFlightControlParameter interface
    * Stop using provider

---
+++ src/kOS/Control/IFlightControlParameter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kOS.Control
+{
+    public interface IFlightControlParameter
+    {
+        bool Enabled { get; }
+        bool IsAutopilot { get; }
+        object Value { get; set; }
+        uint ControlPart { get; }
+        void UpdateState();
+        void UpdateAutopilot(FlightCtrlState c);
+        void EnableControl(SharedObjects shared);
+        void DisableControl(SharedObjects shared);
+        void DisableControl();
+    }
+}
