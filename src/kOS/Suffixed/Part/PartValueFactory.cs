eb2927d7c1be6f078d117287951a1249d27e36ad
hvacengi <hvacengi@gmail.com>
2016-02-10 23:39:20 -0500

    Formatting
    PartValueFactory.cs
    * Formatting changes.

--- src/kOS/Suffixed/Part/PartValueFactory.cs
+++ src/kOS/Suffixed/Part/PartValueFactory.cs
@@ -1,37 +1,37 @@
+using kOS.Safe.Encapsulation;
 using System.Collections.Generic;
 using System.Linq;
-using kOS.Safe.Encapsulation;
 
 namespace kOS.Suffixed.Part
 {
-    public class PartValueFactory 
+    public class PartValueFactory
     {
         public static ListValue Construct(IEnumerable<global::Part> parts, SharedObjects shared)
         {
             var partList = parts.Select(part => Construct(part, shared)).ToList();
             return ListValue.CreateList(partList);
-        } 
+        }
 
         public static ListValue<PartValue> ConstructGeneric(IEnumerable<global::Part> parts, SharedObjects shared)
         {
             var partList = parts.Select(part => Construct(part, shared)).ToList();
             return ListValue<PartValue>.CreateList(partList);
-        } 
+        }
 
         public static PartValue Construct(global::Part part, SharedObjects shared)
         {
             var multiModeEngines = part.Modules.GetModules<MultiModeEngine>();
             if (multiModeEngines.Count > 0)
                 return new EngineValue(part, multiModeEngines.First(), shared);
-            
+
             var moduleEngines = part.Modules.GetModules<ModuleEngines>();
             if (moduleEngines.Count > 0)
                 return new EngineValue(part, new ModuleEngineAdapter(moduleEngines.First()), shared);
-            
+
             var moduleDockingNodes = part.Modules.GetModules<ModuleDockingNode>();
             if (moduleDockingNodes.Count > 0)
                 return new DockingPortValue(moduleDockingNodes.First(), shared);
-            
+
             var moduleEnviroSensors = part.Modules.GetModules<ModuleEnviroSensor>();
             if (moduleEnviroSensors.Count > 0)
                 return new SensorValue(part, moduleEnviroSensors.First(), shared);
