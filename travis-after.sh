6c5e6a0c612d4ebdda2cc83ea17be8648d34e691
Brad White <hvacengi@gmail.com>
2016-02-19 11:35:14 -0500

    Try to get travis to work on pull requests
    .travis.yml
    * Move before_install and after_script logic to external scrpts
    travis-before.sh
    * Replicate library download method from yml file
    * Add logic to download unencrypted dummy tar if encryption not
    supported (PR)
    travis-after.sh
    * replicate logic from yml file.

---
+++ travis-after.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -ev
+
+cd Resources
+rm *.dll
+cd ../
---
+++ travis-before.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ev
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+	echo "Build from within the home repository, encryption enabled."
+	wget --quiet https://github.com/KSP-KOS/KSP_LIB/blob/master/kos-1.0.5.tar.enc?raw=true -O kos-1.0.5.tar.enc
+	openssl aes-256-cbc -K $encrypted_6287ee711a27_key -iv $encrypted_6287ee711a27_iv -in kos-1.0.5.tar.enc -out kos-1.0.5.tar -d
+else
+	echo "Build from pull request outside of the home repository, encryption disabled."
+	wget --quiet https://github.com/KSP-KOS/KSP_LIB/blob/master/kos-1.0.5.tar?raw=true -O kos-1.0.5.tar
+fi
+
+mkdir -p Resources
+tar -xvf kos-1.0.5.tar -C Resources/
