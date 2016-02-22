3ba38bc8a1e1b620997cf2e89a37286e380e15e1
Brad White <hvacengi@gmail.com>
2016-02-22 10:04:52 -0500

    Check to see if make was hiding the error
    script-sphinxdocs.sh
    * Remove -W but keep the command line to see if make was hiding previous
    errors.

--- .ci/travis/script-sphinxdocs.sh
+++ .ci/travis/script-sphinxdocs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd doc
-sphinx-build -b html -q -W -d build/doctrees source gh-pages
+sphinx-build -b html -q -d build/doctrees source gh-pages
+echo $?
+#sphinx-build -b html -q -W -d build/doctrees source gh-pages
 #make html
 cd ../
