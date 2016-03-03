b66606d3e9301f46139814a9d6672044b374788d
hvacengi <hvacengi@gmail.com>
2016-03-03 17:26:07 -0500

    Use environmental variables
    Change references to 1.0.5 into references to ${KSP_VERSION} and define
    the variable

--- .ci/travis/before_script.sh
+++ .ci/travis/before_script.sh
@@ -3,15 +3,15 @@ set -ev
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	echo "Build from within the home repository, encryption enabled."
-	wget --quiet https://github.com/KSP-KOS/KSP_LIB/blob/master/kos-1.0.5.tar.enc?raw=true -O kos-1.0.5.tar.enc
-	openssl aes-256-cbc -K $encrypted_6287ee711a27_key -iv $encrypted_6287ee711a27_iv -in kos-1.0.5.tar.enc -out kos-1.0.5.tar -d
+	wget --quiet https://github.com/KSP-KOS/KSP_LIB/blob/master/kos-${KSP_VERSION}.tar.enc?raw=true -O kos-${KSP_VERSION}.tar.enc
+	openssl aes-256-cbc -K $encrypted_6287ee711a27_key -iv $encrypted_6287ee711a27_iv -in kos-${KSP_VERSION}.tar.enc -out kos-${KSP_VERSION}.tar -d
 else
 	echo "Build from pull request outside of the home repository, encryption disabled."
-	wget --quiet https://github.com/KSP-KOS/KSP_LIB/blob/master/kos-1.0.5.tar?raw=true -O kos-1.0.5.tar
+	wget --quiet https://github.com/KSP-KOS/KSP_LIB/blob/master/kos-${KSP_VERSION}.tar?raw=true -O kos-${KSP_VERSION}.tar
 fi
 
 mkdir -p Resources
-tar -xvf kos-1.0.5.tar -C Resources/
+tar -xvf kos-${KSP_VERSION}.tar -C Resources/
 
 python --version
 pip --version
