b93b6380fd2c4d71187a59fe41566c9f2bfc2692
hvacengi <hvacengi@gmail.com>
2016-03-24 08:40:05 -0400

    Add new CraftTemplate
    CraftTemplate.cs
    * New structure for interacting with template information of craft files
    * Supports access to the name, description, mass, part count, and cost
    of the craft
    * Implements logic for finding a craft file based on the name and
    facility
    * Implements logic for finding all craft files and listing them as
    CraftTemplate
    KUniverseValue.cs
    * New suffix GETCRAFT to get the craft by name and facility
    * New suffix LAUNCHCRAFT to launch a given CraftTemplate
    * New suffix LAUNCHCRAFTFROM to launch a given CraftTemplate from a
    given launch site
    * Launching a craft respect pre flight checks.
    * The check's dialog box cannot be hidden, so there is no way to
    automate dealing with an error (such as insufficient funds).
    * Execution is not currently broken by this command directly, but
    launching a new vessel forces a destruction of the current vessel, and a
    reload if it is within physics range (as opposed to the original vessel
    persisting if it is within physics range as happens when switching the
    active vessel).

---
+++ src/kOS/Suffixed/CraftTemplate.cs
@@ -0,0 +1,133 @@
+﻿using kOS.Safe.Encapsulation;
+using kOS.Safe.Encapsulation.Suffixes;
+using kOS.Safe.Exceptions;
+using System.Collections.Generic;
+using System.IO;
+
+namespace kOS.Suffixed
+{
+    [kOS.Safe.Utilities.KOSNomenclature("CraftTemplate")]
+    public class CraftTemplate : Structure
+    {
+        public static CraftTemplate GetTemplateByName(string name, string facility)
+        {
+            if (string.IsNullOrEmpty(facility))
+            {
+                facility = "<empty>";
+            }
+            if (!string.IsNullOrEmpty(name))
+            {
+                switch (facility.ToLower())
+                {
+                    case ("vab"):
+                        EditorDriver.editorFacility = EditorFacility.VAB;
+                        break;
+
+                    case ("sph"):
+                        EditorDriver.editorFacility = EditorFacility.SPH;
+                        break;
+
+                    default:
+                        throw new KOSException("Failed to load craft.  Facility " + facility + " not recognized, expected \"VAB\" or \"SPH\"");
+                }
+                var path = ShipConstruction.GetSavePath(name);
+                var template = ShipConstruction.LoadTemplate(path);
+                if (template == null)
+                    throw new KOSException("Failed to load craft named \"" + name + "\" from given path:\n  " + path);
+                return new CraftTemplate(path, template);
+            }
+            throw new KOSException("Failed to load craft named " + name + " from facility " + facility);
+        }
+
+        public static ListValue GetAllTemplates()
+        {
+            ListValue ret = new ListValue();
+            var files = GetAllPathStrings();
+            foreach (string path in files)
+            {
+                var template = ShipConstruction.LoadTemplate(path);
+                if (template != null)
+                {
+                    ret.Add(new CraftTemplate(path, template));
+                }
+            }
+            return ret;
+        }
+
+        private static List<string> GetAllPathStrings()
+        {
+            var ret = new List<string>();
+            string path1 = KSPUtil.GetOrCreatePath("Ships/VAB");
+            string path2 = KSPUtil.GetOrCreatePath("Ships/SPH");
+            string path3 = KSPUtil.GetOrCreatePath("saves/" + HighLogic.SaveFolder + "/Ships/VAB");
+            string path4 = KSPUtil.GetOrCreatePath("saves/" + HighLogic.SaveFolder + "/Ships/SPH");
+            ret.AddRange(Directory.GetFiles(path1));
+            ret.AddRange(Directory.GetFiles(path2));
+            ret.AddRange(Directory.GetFiles(path3));
+            ret.AddRange(Directory.GetFiles(path4));
+            return ret;
+        }
+
+        private string path;
+        private ShipTemplate template;
+
+        public CraftTemplate(string filePath)
+        {
+            path = filePath;
+            template = ShipConstruction.LoadTemplate(filePath);
+            if (template == null)
+                throw new KOSException("Failed to load template from given path:\n  " + filePath);
+            InitializeSuffixes();
+        }
+
+        public CraftTemplate(string filePath, ShipTemplate temp)
+        {
+            path = filePath;
+            template = temp;
+            if (template == null)
+                throw new KOSException("Failed to load template from given path:\n  " + filePath);
+            InitializeSuffixes();
+        }
+
+        private void InitializeSuffixes()
+        {
+            AddSuffix("NAME", new Suffix<StringValue>(() => Name));
+            AddSuffix("DESCRIPTION", new Suffix<StringValue>(() => Description));
+            AddSuffix("EDITOR", new Suffix<StringValue>(() => Editor));
+            AddSuffix("LAUNCHFACILITY", new Suffix<StringValue>(() => LaunchFacility));
+            AddSuffix("MASS", new Suffix<ScalarValue>(() => Mass));
+            AddSuffix("COST", new Suffix<ScalarValue>(() => Cost));
+            AddSuffix("PARTCOUNT", new Suffix<ScalarValue>(() => PartCount));
+        }
+
+        public ShipTemplate InnerTemplate { get { return template; } }
+
+        public string Name { get { return template.shipName; } }
+
+        public string Description { get { return template.shipDescription; } }
+
+        public string FilePath { get { return path; } }
+
+        public string Editor
+        {
+            get
+            {
+                return template.shipType == (int)EditorFacility.SPH ? "SPH" : "VAB";
+            }
+        }
+
+        public string LaunchFacility
+        {
+            get
+            {
+                return template.shipType == (int)EditorFacility.SPH ? "Runway" : "LaunchPad";
+            }
+        }
+
+        public float Mass { get { return template.totalMass; } }
+
+        public double Cost { get { return template.totalCost; } }
+
+        public double PartCount { get { return template.partCount; } }
+    }
+}
