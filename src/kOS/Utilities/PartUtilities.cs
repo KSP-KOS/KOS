dc1d0e9390fa66ddef558e49ffc5293807af67e7
hvacengi <hvacengi@gmail.com>
2016-05-11 11:07:41 -0400

    Fix part mass modifications
    Fixes #1643
    
    Fixes #1115
    
    kOSProcessor.cs
    * Fix implementation of IPartMassModifier so that kOS does not change
    the part's mass directly.
    * Revise base mass/cost logic to allow for adding mass and cost when the
    module is added to other parts.
    * Pulled mass/cost multiplication factors out of constants and into the
    part definition.  This way the part itself can define how much mass to
    add (since the KAL9000's mass added is not in line with the initial
    mass).
    * Removed old logic for establishing the base cost based on additional
    cost.
    
    PartUtilities.cs
    * Revise mass handling for parts so that physics-less parts are
    accounted for
    * Revise wet and dry mass calculation to use the new part mass
    calculation.  This should make both the ship and part masses for each
    correct.

--- src/kOS/Utilities/PartUtilities.cs
+++ src/kOS/Utilities/PartUtilities.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using kOS.Safe.Encapsulation;
+using System;
 
 namespace kOS.Utilities
 {
@@ -6,7 +7,17 @@ namespace kOS.Utilities
     {
         public static float CalculateCurrentMass(this Part part)
         {
-            return part.HasPhysics() ? part.mass + part.GetResourceMass() : 0;
+            if (part.HasPhysics())
+            {
+                if (part.rb != null)
+                {
+                    // rb mass is one physics tick behind.  Use part.GetPhysicslessChildMass() if the
+                    // delay becomes a significant problem, but this should be good 99% of the time.
+                    return part.rb.mass;
+                }
+            }
+            // default to zero if the rigid body is not yet updated, or the part is physics-less
+            return 0;
         }
 
         public static bool HasPhysics(this Part part)
@@ -24,14 +35,17 @@ namespace kOS.Utilities
 
         public static float GetDryMass(this Part part)
         {
-            return !part.HasPhysics() ? 0 : part.mass;
+            // this will technically have an oportunity to return a negative wet mass
+            // if the part is physics-less, but that option is intended for small part
+            // to help with the physics calculation, not tanks of fuel.
+            return part.CalculateCurrentMass() - part.resourceMass;
         }
 
         public static float GetWetMass(this Part part)
         {
-            if (!part.HasPhysics()) return 0;
-
-            float mass = part.mass;
+            // See the note above regarding negative dry mass, the wet mass may net to
+            // zero in the same case.  Again, highly unlikely.
+            float mass = part.GetDryMass();
 
             for (int index = 0; index < part.Resources.Count; ++index)
             {
