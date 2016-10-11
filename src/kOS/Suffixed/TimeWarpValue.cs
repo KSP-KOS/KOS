5fd7c8343e9b3bafc2fb9d3d16df78ba505bf0ce
Brad White <hvacengi@gmail.com>
2016-10-10 20:31:17 -0400

    Small white space clean up
    Mostly remove extra white space from code.

--- src/kOS/Suffixed/TimeWarpValue.cs
+++ src/kOS/Suffixed/TimeWarpValue.cs
@@ -1,8 +1,8 @@
 ﻿using kOS.Safe.Encapsulation;
 using kOS.Safe.Encapsulation.Suffixes;
 using kOS.Safe.Utilities;
-using UnityEngine;
 using System;
+using UnityEngine;
 
 namespace kOS.Suffixed
 {
@@ -14,12 +14,12 @@ namespace kOS.Suffixed
     public class TimeWarpValue : Structure
     {
         private static TimeWarpValue selfSingleInstance = null;
-        
+
         private TimeWarpValue()
         {
             InitializeSuffixes();
         }
-        
+
         public static TimeWarpValue Instance
         {
             get
@@ -29,26 +29,26 @@ namespace kOS.Suffixed
                 return selfSingleInstance;
             }
         }
-        
+
         private void InitializeSuffixes()
         {
-            AddSuffix("RATE", new SetSuffix<ScalarValue>(GetRate,SetRate));
+            AddSuffix("RATE", new SetSuffix<ScalarValue>(GetRate, SetRate));
             AddSuffix("RATELIST", new Suffix<ListValue<ScalarValue>>(GetRatesList));
             AddSuffix("RAILSRATELIST", new Suffix<ListValue<ScalarValue>>(() => GetRatesList(TimeWarp.Modes.HIGH)));
             AddSuffix("PHYSICSRATELIST", new Suffix<ListValue<ScalarValue>>(() => GetRatesList(TimeWarp.Modes.LOW)));
-            AddSuffix("MODE", new SetSuffix<StringValue>(GetModeAsString,SetModeAsString));
-            AddSuffix("WARP", new SetSuffix<ScalarIntValue>(GetWarp,SetWarp));
+            AddSuffix("MODE", new SetSuffix<StringValue>(GetModeAsString, SetModeAsString));
+            AddSuffix("WARP", new SetSuffix<ScalarIntValue>(GetWarp, SetWarp));
             AddSuffix("WARPTO", new OneArgsSuffix<ScalarValue>(WarpTo));
             AddSuffix("CANCELWARP", new NoArgsVoidSuffix(CancelWarp));
             AddSuffix("PHYSICSDELTAT", new Suffix<ScalarValue>(GetDeltaT));
             AddSuffix("ISSETTLED", new Suffix<BooleanValue>(IsWarpSettled));
         }
-        
+
         public ScalarValue GetRate()
         {
             return TimeWarp.CurrentRate;
         }
-        
+
         // Set the rate to the nearest rate to the value given that the user interface
         // normally allows.  (i.e. if you tell it to set the rate to 90x, it will round
         // to 100x)  Note that the underlying API allows any in-between warp value, but
@@ -59,27 +59,27 @@ namespace kOS.Suffixed
         {
             float wantRate = desiredRate;
             float[] rateArray = GetRateArrayForMode(TimeWarp.WarpMode);
-            
+
             // Walk the list of possible rates, settling on the one
             // that is closest to the desired rate (has smallest error
             // difference):
             float error = float.MaxValue;
             int index;
-            for (index = 0 ; index < rateArray.Length ; ++index)
+            for (index = 0; index < rateArray.Length; ++index)
             {
                 float nextError = Mathf.Abs(wantRate - rateArray[index]);
                 if (nextError > error)
                     break;
                 error = nextError;
             }
-            SetWarpRate(index-1, rateArray.Length-1);
+            SetWarpRate(index - 1, rateArray.Length - 1);
         }
-        
+
         public BooleanValue IsPhysicsWarping()
         {
             return TimeWarp.WarpMode == TimeWarp.Modes.LOW;
         }
-        
+
         public StringValue GetModeAsString()
         {
             switch (TimeWarp.WarpMode)
@@ -92,7 +92,7 @@ namespace kOS.Suffixed
                     return "UNDEFINED"; // This should never happen unless SQUAD adds more to the Modes enum.
             }
         }
-        
+
         public void SetModeAsString(StringValue modeString)
         {
             switch (modeString.ToUpper())
@@ -101,18 +101,18 @@ namespace kOS.Suffixed
                     TimeWarp.fetch.Mode = TimeWarp.Modes.LOW;
                     break;
                 case "RAILS":
-                    TimeWarp.fetch.Mode = TimeWarp.Modes.HIGH;                
+                    TimeWarp.fetch.Mode = TimeWarp.Modes.HIGH;
                     break;
                 default:
                     throw new Exception(string.Format("WARP MODE '{0}' is not valid", modeString.ToString()));
             }
         }
-        
+
         public ScalarIntValue GetWarp()
         {
             return TimeWarp.CurrentRateIndex;
         }
-        
+
         public void SetWarp(ScalarIntValue newIndex)
         {
             int newRate = newIndex;
@@ -128,12 +128,12 @@ namespace kOS.Suffixed
                     throw new Exception(string.Format("WARP MODE {0} unknown to kOS - did KSP's API change?", TimeWarp.WarpMode.ToString()));
             }
         }
-        
+
         public ScalarValue GetDeltaT()
         {
             return TimeWarp.fixedDeltaTime;
         }
-        
+
         public ListValue<ScalarValue> GetRatesList()
         {
             return GetRatesList(TimeWarp.WarpMode);
@@ -141,46 +141,49 @@ namespace kOS.Suffixed
 
         public ListValue<ScalarValue> GetRatesList(TimeWarp.Modes warpMode)
         {
-            float [] ratesArray = GetRateArrayForMode(warpMode);
-            
+            float[] ratesArray = GetRateArrayForMode(warpMode);
+
             ListValue<ScalarValue> ratesKOSList = new ListValue<ScalarValue>();
-            
+
             // Have to convert the elements one at a time from (float) to (ScalarDoubleValue):
             foreach (float val in ratesArray)
                 ratesKOSList.Add(val);
-            
+
             return ratesKOSList;
         }
-        
+
         public BooleanValue IsWarpSettled()
         {
             float expectedRate = GetRateArrayForMode(TimeWarp.WarpMode)[TimeWarp.CurrentRateIndex];
-            
+
             // The comparison has to have a large-ish epsilon because sometimes the
             // current rate will eventually settle steady on something like 99.9813 or so
             // for 100x. (far bigger than one would expect for a normal floating point
             // equality epsilon):
-            if ( Mathf.Abs(expectedRate - TimeWarp.CurrentRate) < 0.05)
+            if (Mathf.Abs(expectedRate - TimeWarp.CurrentRate) < 0.05)
                 return true;
             else
                 return false;
         }
-        
+
         public void WarpTo(ScalarValue timeStamp)
         {
             TimeWarp.fetch.WarpTo(timeStamp.GetDoubleValue());
         }
 
+        /// <summary>
+        /// Cancel any current auto-warping and reset the warp to default.
+        /// </summary>
         public void CancelWarp()
         {
             TimeWarp.fetch.CancelAutoWarp();
             SetWarp(0);
         }
-        
+
         // Return which of the two rates arrays is to be used for the given mode:
         private float[] GetRateArrayForMode(TimeWarp.Modes whichMode)
         {
-            float [] rateArray;
+            float[] rateArray;
             switch (whichMode)
             {
                 case TimeWarp.Modes.HIGH:
@@ -194,7 +197,7 @@ namespace kOS.Suffixed
             }
             return rateArray;
         }
-        
+
         private static void SetWarpRate(int newRate, int maxRate)
         {
             var clampedValue = Mathf.Clamp(newRate, 0, maxRate);
@@ -204,6 +207,5 @@ namespace kOS.Suffixed
             }
             TimeWarp.SetRate(clampedValue, false);
         }
-        
     }
 }
