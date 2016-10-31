c0e2c333e29363ec6676fc10c854967a622f6ca7
hvacengi <hvacengi@gmail.com>
2016-10-31 16:39:06 -0400

    Bindings for connections and some documentation
    ControlConnection.cs & HomeConnection.cs
    * New classes for home and control connection checking
    * Allows getting the delay, connection status, and destination
    information
    * Attempting to send a message throws an error
    
    BindingsUniverse.cs
    * Add binding to access ControlConnection and HomeConnection, with the
    value itself cached.
    
    kOS.csproj
    * Add references to new files
    
    IConnectivityManager.cs
    * Add some documentation to the interface so that it

---
+++ src/kOS/Communication/ControlConnection.cs
@@ -0,0 +1,42 @@
+﻿using kOS.Safe.Communication;
+using kOS.Safe.Encapsulation;
+
+namespace kOS.Communication
+{
+    [Safe.Utilities.KOSNomenclature("Connection", KOSToCSharp = false)]
+    public class ControlConnection : Connection
+    {
+        private SharedObjects shared;
+
+        public ControlConnection(SharedObjects sharedObj)
+        {
+            shared = sharedObj;
+        }
+
+        public override bool Connected
+        {
+            get
+            {
+                return ConnectivityManager.HasControl(shared.Vessel);
+            }
+        }
+
+        public override double Delay
+        {
+            get
+            {
+                return ConnectivityManager.GetDelayToControl(shared.Vessel);
+            }
+        }
+
+        protected override Structure Destination()
+        {
+            return new StringValue("CONTROL");
+        }
+
+        protected override BooleanValue SendMessage(Structure content)
+        {
+            throw new Safe.Exceptions.KOSException("kOS does not support sending messages to the control source");
+        }
+    }
+}
--- src/kOS/Communication/IConnectivityManager.cs
+++ src/kOS/Communication/IConnectivityManager.cs
@@ -1,19 +1,86 @@
-﻿using System;
-
-namespace kOS.Communication
+﻿namespace kOS.Communication
 {
     public interface IConnectivityManager
     {
+        /// <summary>
+        /// Intended for internal use, so that the ConnectivityManager class can determine if
+        /// the module implementing this interface is currently able to run.
+        /// </summary>
         bool IsEnabled { get; }
-        bool NeedAutopilotResubscribe { get; }
-        double GetDelay(Vessel vessel1, Vessel vessel2);
-        double GetDelayToHome(Vessel vessel);
-        double GetDelayToControl(Vessel vessel);
-        bool HasConnectionToHome(Vessel vessel);
-        bool HasControl(Vessel vessel);
-        bool HasConnection(Vessel vessel1, Vessel vessel2);
-        void AddAutopilotHook(Vessel vessel, FlightInputCallback hook);
-        void RemoveAutopilotHook(Vessel vessel, FlightInputCallback hook);
 
+        /// <summary>
+        /// Classes implementing this interface should return true for this property if autopilot
+        /// event subscription can be cleared without notifying our classes and as such they
+        /// should expect to periodically "resubscribe" to the events.
+        /// </summary>
+        bool NeedAutopilotResubscribe { get; }
+
+        /// <summary>
+        /// Get the signal delay between two given vessels.
+        /// </summary>
+        /// <param name="vessel1"></param>
+        /// <param name="vessel2"></param>
+        /// <returns>The delay in seconds.  If there is no connection, this should return -1</returns>
+        double GetDelay(Vessel vessel1, Vessel vessel2);
+
+        /// <summary>
+        /// Get the signal delay between the given vessel and "Home".  "Home" is a concept inherited from
+        /// stock CommNet and represents KSC or other ground stations.  It does not represent a connection
+        /// to a command station.  This concept of "home" should be adapted, but remain essentially the
+        /// same concept, for other mods that may support additional ground stations.
+        /// </summary>
+        /// <param name="vessel"></param>
+        /// <returns>The delay in seconds.  If there is no connection, this should return -1</returns>
+        double GetDelayToHome(Vessel vessel);
+
+        /// <summary>
+        /// Get the signal delay between the given vessel and the control source.  This control source
+        /// may be the "home" discussed in the GetDelayToHome summary, or any other command station
+        /// allowing control of the given vessel.
+        /// </summary>
+        /// <param name="vessel"></param>
+        /// <returns>The delay in seconds.  If there is no connection, this should return -1</returns>
+        double GetDelayToControl(Vessel vessel);
+
+        /// <summary>
+        /// Determine if there is a connection between the given vessel and "home".  See the commentary
+        /// on "home" in the summary of GetDelayToHome.
+        /// </summary>
+        /// <param name="vessel"></param>
+        /// <returns>True if there is a connection, False if no connection is available</returns>
+        bool HasConnectionToHome(Vessel vessel);
+
+        /// <summary>
+        /// Determine if there is a connection to a control source.  See the comentary in the summary
+        /// of GetDelayToControl
+        /// </summary>
+        /// <param name="vessel"></param>
+        /// <returns>True if there is a connection, False if no connection is available</returns>
+        bool HasControl(Vessel vessel);
+
+        /// <summary>
+        /// Determine if there is a connection betweeen the two given vessels.
+        /// </summary>
+        /// <param name="vessel1"></param>
+        /// <param name="vessel2"></param>
+        /// <returns>The delay in seconds.  If there is no connection, this should return -1</returns>
+        bool HasConnection(Vessel vessel1, Vessel vessel2);
+
+        /// <summary>
+        /// This method subscribes to autopilot events to allow kOS to control things like steering and
+        /// throttle.  This allows another mod to block the functionality of the stock events, while
+        /// still leting mods like kOS serve their autopilot purpose.
+        /// </summary>
+        /// <param name="vessel"></param>
+        /// <param name="hook"></param>
+        void AddAutopilotHook(Vessel vessel, FlightInputCallback hook);
+
+        /// <summary>
+        /// This method unsubscribes to autopilot events, effectively releasing kOS's control over a vessel
+        /// as well as preventing issues with stale references.
+        /// </summary>
+        /// <param name="vessel"></param>
+        /// <param name="hook"></param>
+        void RemoveAutopilotHook(Vessel vessel, FlightInputCallback hook);
     }
 }
