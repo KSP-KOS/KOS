29c40584aeb7e3993ca1421b9dfc31353d000033
hvacengi <hvacengi@gmail.com>
2016-11-30 14:23:03 -0500

    Review of KOSNumberParseException
    General format update, and expose terse format for future use of other
    objects.

--- src/kOS.Safe/Exceptions/KOSNumberParseException.cs
+++ src/kOS.Safe/Exceptions/KOSNumberParseException.cs
@@ -1,22 +1,19 @@
-﻿using System;
-using kOS.Safe.Utilities;
-
-namespace kOS.Safe.Exceptions
+﻿namespace kOS.Safe.Exceptions
 {
     /// <summary>
     /// Indicates that KOS attempted to turn a string into a number but the
     /// string was not in a recognized number format.
     /// </summary>
-    public class KOSNumberParseException: KOSException
+    public class KOSNumberParseException : KOSException
     {
-        private const string TERSE_MSG_FMT = "{0} is not in a recognized number format.";
+        public const string TERSE_MSG_FMT = "\"{0}\" is not in a recognized number string format.";
 
-        public override string VerboseMessage { get{ return BuildVerboseMessage(); } }
+        public override string VerboseMessage { get { return BuildVerboseMessage(); } }
 
-        public override string HelpURL { get{ return "";} }
+        public override string HelpURL { get { return ""; } }
 
         private string attemptedString;
-        
+
         public KOSNumberParseException(string attemptedString)
             : base(string.Format(TERSE_MSG_FMT, attemptedString))
         {
@@ -25,18 +22,17 @@ namespace kOS.Safe.Exceptions
 
         private string BuildVerboseMessage()
         {
-            const string VERBOSE_TEXT = "kOS attempted to convert the string:\n"+
-                "  \"{0}\"\n"+
-                "into a Scalar number, but the format of the string isn't\n"+
-                "recognizable as a number.\n"+
-                "The recognizable format is:\n"+
-                "  (optional sign), then\n"+
-                "  (mandatory digits), then\n"+
-                "  (optional decimal dot, then optional digits), then\n"+
+            const string VERBOSE_TEXT = "kOS attempted to convert the string:\n" +
+                "  \"{0}\"\n" +
+                "into a Scalar number, but the format of the string isn't\n" +
+                "recognizable as a number.\n" +
+                "The recognizable format is:\n" +
+                "  (optional sign), then\n" +
+                "  (mandatory digits), then\n" +
+                "  (optional decimal dot, then optional digits), then\n" +
                 "  (optional letter 'e', then optional sign, then digits)\n";
 
-            return String.Format(VERBOSE_TEXT, attemptedString);
+            return string.Format(VERBOSE_TEXT, attemptedString);
         }
     }
 }
-
