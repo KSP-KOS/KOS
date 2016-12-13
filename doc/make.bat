172b0209507c598f1d8eaa27dd70b82f541f25fc
WarwickAllison <warwick@allipaca.com>
2016-12-13 16:52:53 +1000

    Stop sphinxbuild help spamming stdout on version test.

--- doc/make.bat
+++ doc/make.bat
@@ -47,7 +47,7 @@ if "%1" == "clean" (
 )
 
 
-%SPHINXBUILD% 2> nul
+%SPHINXBUILD% 2> nul >nul
 if errorlevel 9009 (
     echo.
     echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
