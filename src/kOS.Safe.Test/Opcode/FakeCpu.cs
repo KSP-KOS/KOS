1d6f952f3f9a26316062e114ad71393adcf0709e
Tom Holmes <tholmes150@gmail.com>
2017-10-21 20:39:05 -0700

    Fix test FakeCpu

--- src/kOS.Safe.Test/Opcode/FakeCpu.cs
+++ src/kOS.Safe.Test/Opcode/FakeCpu.cs
@@ -35,12 +35,12 @@ namespace kOS.Safe.Test.Opcode
             return fakeStack.Pop();
         }
 
-        public void PushAboveStack(object thing)
+        public void PushScopeStack(object thing)
         {
             throw new NotImplementedException();
         }
 
-        public object PopAboveStack(int howMany)
+        public object PopScopeStack(int howMany)
         {
             throw new NotImplementedException();
         }
