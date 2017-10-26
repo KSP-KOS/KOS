6e158479db195a712b07aad78e8e867b85a19384
Dunbaratu <madings@gmail.com>
2017-10-25 20:23:24 -0500

    Added KOS stack exception type.

---
+++ src/kOS.Safe/Exceptions/KOSStackOverflowException.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace kOS.Safe.Exceptions
+{
+    public class KOSStackOverflowException : KOSException
+    {
+        private const string formatMsg = "%s Stack Overflow";
+        private const string verboseMsg = "This is usually caused by excessive recursion in the script.";
+       
+        public KOSStackOverflowException(string whatStack) : base(string.Format(formatMsg, whatStack))
+        {
+        }
+            
+        public override string VerboseMessage
+        {
+            get { return string.Format("%s %s", base.Message, verboseMsg); }
+        }
+    }
+}
+
