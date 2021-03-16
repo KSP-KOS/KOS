0ce51b7d2cf807165c9bed1ed90cf9d3eff56c36
Dunbaratu <madings@gmail.com>
2021-03-16 06:36:03 -0500

    Fixes #2899 terrain raycast now ingores parts.
    The fix I went with was to detect whether or not a raycast hit
    is inside a Part.  If it is, then start a new raycast just past
    the hit and keep looking.
    
    The solution involved writing a wrapper around Raycast that
    includes this extra logic, such that it could be lifted and
    put in a utility library later if any other classes need to
    use it.

--- src/kOS.Safe/Exceptions/KOSYouShouldNeverSeeThisException.cs
+++ src/kOS.Safe/Exceptions/KOSYouShouldNeverSeeThisException.cs
@@ -5,7 +5,7 @@ namespace kOS.Safe.Exceptions
 {
     public class KOSYouShouldNeverSeeThisException : KOSException
     {
-        public KOSYouShouldNeverSeeThisException(string message) : base("This is an error endusers should never see, if you see this please report it to the kOS devs:\r\n    " + message)
+        public KOSYouShouldNeverSeeThisException(string message) : base("This is an error end users should never see. If you see this please report it on the kOS github:\r\n    " + message)
         {
         }
     }
