7b23bda6f22df2319feedf0592ec806067ad25b8
Jon Cable <jonnyothan@gmail.com>
2021-08-21 14:13:11 -0400

    add a mechanism for addons to create specialized PartModuleFields

--- src/kOS/Suffixed/PartModuleField/PartModuleFieldsFactory.cs
+++ src/kOS/Suffixed/PartModuleField/PartModuleFieldsFactory.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using kOS.Safe.Encapsulation;
 using kOS.AddOns.RemoteTech;
@@ -11,6 +11,8 @@ namespace kOS.Suffixed.PartModuleField
     /// </summary>
     public class PartModuleFieldsFactory 
     {
+        public delegate PartModuleFields ConstructPartModuleFieldsMethod(PartModule partModule, SharedObjects shared);
+
         public static ListValue Construct(IEnumerable<PartModule> modules, SharedObjects shared)
         {
             var list = modules.Select(mod => Construct(mod, shared)).ToList();
@@ -21,20 +23,34 @@ namespace kOS.Suffixed.PartModuleField
         {
             var moduleGimbal = mod as ModuleGimbal;
             if (moduleGimbal != null)
+            {
                 return new GimbalFields(moduleGimbal, shared);
+            }
 
             var processor = mod as kOSProcessor;
-
-            if (processor != null) {
+            if (processor != null)
+            {
                 return new kOSProcessorFields(processor, shared);
             }
 
-            if (mod.moduleName.Equals(RemoteTechAntennaModuleFields.RTAntennaModule)) {
+            // see if any addons have registered a constructor for this module
+            var constructionMethod = constructionMethods[mod.moduleName];
+            if (constructionMethod != null)
+            {
+                var moduleFields = constructionMethod(mod, shared);
+                if (moduleFields != null)
+                {
+                    return moduleFields;
+                }
+            }
+
+            // this should probably get moved to the registration system
+            if (mod.moduleName.Equals(RemoteTechAntennaModuleFields.RTAntennaModule))
+            {
                 return new RemoteTechAntennaModuleFields(mod, shared);
             }
 
             var scienceExperimentFields = ScienceExperimentFieldsFactory.Construct(mod, shared);
-
             if (scienceExperimentFields != null)
             {
                 return scienceExperimentFields;
@@ -42,5 +58,13 @@ namespace kOS.Suffixed.PartModuleField
 
             return new PartModuleFields(mod, shared);
         }
+
+        public static void RegisterConstructionMethod(string moduleName, ConstructPartModuleFieldsMethod method)
+        {
+            constructionMethods[moduleName] = method;
+        }
+
+        // maps a module name to a function
+        protected static Dictionary<string, ConstructPartModuleFieldsMethod> constructionMethods = new Dictionary<string, ConstructPartModuleFieldsMethod>();
     }
 }
