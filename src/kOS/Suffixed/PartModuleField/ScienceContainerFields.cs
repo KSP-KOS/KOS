693c7a2a79c6615f229bd870a4e7bf4a56b35dd1
Will McVay <citizenslave@gmail.com>
2022-04-26 22:03:26 -0400

    Add ScienceContainerFields class and instantiate

---
+++ src/kOS/Suffixed/PartModuleField/ScienceContainerFields.cs
@@ -0,0 +1,48 @@
+using System;
+using kOS.Safe.Encapsulation.Suffixes;
+using kOS.Safe.Exceptions;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+using kOS.Safe.Encapsulation;
+
+namespace kOS.Suffixed.PartModuleField {
+    [kOS.Safe.Utilities.KOSNomenclature("ScienceContainerModule")]
+    public class ScienceContainerFields : PartModuleFields {
+        protected global::Part part;
+        protected IScienceDataContainer container;
+        public ScienceContainerFields(PartModule module, SharedObjects shared) : base(module, shared) {
+            this.container = module as IScienceDataContainer;
+            part = module.part;
+
+            if (container == null) {
+                throw new KOSException("This module is not a science data container");
+            }
+
+            InitializeSuffixes();
+        }
+
+        private void InitializeSuffixes() {
+            AddSuffix("HASDATA", new Suffix<BooleanValue>(() => HasData(), "Does this experiment have any data stored"));
+            AddSuffix("DATA", new Suffix<ListValue>(Data, "Does this experiment have any data stored"));
+            AddSuffix("DUMPDATA", new OneArgsSuffix<ScalarIntValue>(DumpData));
+        }
+
+        public virtual void DumpData(ScalarIntValue index) {
+            container.DumpData(container.GetData()[index]);
+        }
+
+        public virtual bool HasData() {
+            return container.GetData().Any();
+        }
+
+        public virtual ListValue Data() {
+            return new ListValue(container.GetData().Select(s => new ScienceDataValue(s, part)).Cast<Structure>());
+        }
+
+        public new string ToString() {
+            return "SCIENCE CONTAINER";
+        }
+    }
+}
+
--- src/kOS/Suffixed/PartModuleField/ScienceExperimentFieldsFactory.cs
+++ src/kOS/Suffixed/PartModuleField/ScienceExperimentFieldsFactory.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using kOS.Safe.Utilities;
 using kOS.AddOns.OrbitalScience;
 
