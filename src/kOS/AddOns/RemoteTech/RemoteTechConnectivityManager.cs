9fa2d26c715e59992353c80535ca24df33c476fc
KerballOne <KerballOne@outlook.com>
2025-08-09 13:45:37 -0600

    Fix RemoteTech API call for connection to unloaded vessels

--- src/kOS/AddOns/RemoteTech/RemoteTechConnectivityManager.cs
+++ src/kOS/AddOns/RemoteTech/RemoteTechConnectivityManager.cs
@@ -42,6 +42,10 @@ namespace kOS.AddOns.RemoteTech
             if (!(RemoteTechHook.IsAvailable()))
                 return -1; // default to no connection if RT itself isn't available.
             double delay = RemoteTechHook.Instance.GetSignalDelayToSatellite(vessel1.id, vessel2.id);
+            if (Double.IsPositiveInfinity(delay) || delay < 0)
+            {
+                delay = RemoteTechHook.Instance.GetShortestSignalDelay(vessel2.id);
+            }
             return Double.IsPositiveInfinity(delay) ? -1 : delay;
         }
 
