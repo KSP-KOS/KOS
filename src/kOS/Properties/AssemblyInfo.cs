ca45fcc65ad2a02127f79cdfae8d8faf63ed701b
JonnyOThan <jonnyothan@gmail.com>
2025-09-02 11:04:18 -0400

    update to KSPBT 0.0.4

--- src/kOS/Properties/AssemblyInfo.cs
+++ src/kOS/Properties/AssemblyInfo.cs
@@ -4,11 +4,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("kOS")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("kOS")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -21,16 +16,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("324da3fe-391b-421d-93b7-29499dcf9ef3")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: KSPAssembly("kOS", 1, 3)]
